6c410a99b0db8728afe735c8f8ae3f5b6fde4533
Vimal Kumar Yadav <vimalyadavkr001@gmail.com>
2025-03-13 22:16:50 +0530

    completed function in python

---
+++ functions.py
@@ -0,0 +1,19 @@
+def greet_function(name, age):
+    print("Welcome " + name + " you are " + age + " years old")
+
+
+name = input("Enter your name : ")
+age = input("Enter your age : ")
+greet_function(name, age)
+
+
+def add(num1, num2):
+    return num1 + num2
+
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+print(add(num1, num2))
+
+
+
+
