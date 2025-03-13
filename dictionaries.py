0d245c0eb74af7ccb198ac58535528336dac0209
Vimal Kumar Yadav <vimalyadavkr001@gmail.com>
2025-03-13 22:39:35 +0530

    completed dictionaries in python

---
+++ dictionaries.py
@@ -0,0 +1,4 @@
+my_dict = {"name": "Vimal", "nationality": "Indian"}
+print(my_dict["name"])
+# duplicates not allowed if we put two keys of same name then it only considers the last one
+print(len(my_dict))
