a6bda021cc81eb0a0d9db56a5910162f19ede47c
Vimal Kumar Yadav <vimalyadavkr001@gmail.com>
2025-03-14 22:46:34 +0530

    completed basic

---
+++ list_in_2d.py
@@ -0,0 +1,16 @@
+my_list = [
+    [1, 2, 3, 4], 
+    [5, 6, 7, 8],
+    [10, 11, 12, 13]
+]
+print(my_list)
+
+'''
+for lists in my_list:
+    print(lists)
+    string = ""
+    for num in lists:
+        string += str(num) + " "
+    print(string)
+
+'''
